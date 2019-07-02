b662b776d50436b256707441ccbf16e521b417f0
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-07-02 12:54:50 +0200

    Add keep_alive option

--- lib/ibm_speech_to_text/client.ex
+++ lib/ibm_speech_to_text/client.ex
@@ -18,6 +18,7 @@ defmodule IBMSpeechToText.Client do
   ## Options
 
   * `:stream_to` - pid of the process that will receive recognition results, defaults to the caller of `start_link/3`
+  * `:keep_alive` - if set to true, the client will automatically reconnect to the API after timeout (IBM API will close the connection after 30 seconds of silence or no data). False by default.
   * Recognition parameters (such as `:model`) described in [IBM Cloud docs](https://cloud.ibm.com/apidocs/speech-to-text#WSRecognizeMethod)
 
   ## Example
@@ -54,8 +55,14 @@ defmodule IBMSpeechToText.Client do
   end
 
   defp do_start(function, api_url, api_key, opts) do
-    {stream_to, endpoint_opts} = Keyword.pop(opts, :stream_to, self())
-    apply(GenServer, function, [__MODULE__, [api_url, api_key, stream_to, endpoint_opts]])
+    {client_opts, endpoint_opts} = Keyword.split(opts, [:stream_to, :keep_alive])
+
+    client_opts =
+      client_opts
+      |> Keyword.put_new(:stream_to, self())
+      |> Keyword.put_new(:keep_alive, false)
+
+    apply(GenServer, function, [__MODULE__, [api_url, api_key, client_opts, endpoint_opts]])
   end
 
   @doc """
@@ -83,17 +90,10 @@ defmodule IBMSpeechToText.Client do
   end
 
   @impl true
-  def init([api_url, api_key, stream_to, endpoint_opts]) do
+  def init([api_url, api_key, client_opts, endpoint_opts]) do
     with {:ok, conn_pid} <- :gun.open(api_url, Util.ssl_port(), Util.ssl_connection_opts()) do
       monitor = Process.monitor(conn_pid)
       task = Task.async(Token, :get, [api_key])
-
-      state = %{
-        conn_pid: conn_pid,
-        conn_monitor: monitor,
-        stream_to: stream_to,
-        ws_ref: nil
-      }
 
       ws_path =
         case endpoint_opts do
@@ -101,17 +101,28 @@ defmodule IBMSpeechToText.Client do
           _ -> @endpoint_path <> "?" <> URI.encode_query(endpoint_opts)
         end
 
-      {:ok, state, {:continue, {:init, task, ws_path}}}
+      state = %{
+        api_key: api_key,
+        conn_pid: conn_pid,
+        conn_monitor: monitor,
+        stream_to: client_opts[:stream_to],
+        keep_alive: client_opts[:keep_alive],
+        token: nil,
+        ws_path: ws_path,
+        ws_ref: nil
+      }
+
+      {:ok, state, {:continue, {:init, task}}}
     end
   end
 
   @impl true
-  def handle_continue({:init, task, ws_path}, %{conn_pid: conn_pid} = state) do
+  def handle_continue({:init, task}, %{conn_pid: conn_pid, ws_path: ws_path} = state) do
     with {:ok, _protocol} <- :gun.await_up(conn_pid),
          {:ok, token} <- Task.await(task),
          ws_ref = :gun.ws_upgrade(conn_pid, ws_path, [token |> Token.auth_header()]),
          :ok <- await_upgrade(conn_pid, ws_ref) do
-      {:noreply, %{state | ws_ref: ws_ref}}
+      {:noreply, %{state | token: token, ws_ref: ws_ref}}
     else
       {:error, reason} ->
         raise "Error while initializing connection: #{inspect(reason)}"
@@ -148,7 +159,11 @@ defmodule IBMSpeechToText.Client do
         raise "Error while decoding response: #{Jason.DecodeError.message(error)}"
 
       {:error, error} ->
-        raise "Received error over websocket: #{error}"
+        if error =~ "timed out" and state.keep_alive do
+          {:noreply, reconnect(state)}
+        else
+          raise "Received error over websocket: #{error}"
+        end
     end
   end
 
@@ -189,6 +204,25 @@ defmodule IBMSpeechToText.Client do
       {:gun_upgrade, ^conn_pid, ^ws_ref, ["websocket"], _} -> :ok
     after
       5000 -> raise "Timeout while waiting for connection upgrade"
+    end
+  end
+
+  defp reconnect(%{conn_pid: conn_pid} = state) do
+    token_res =
+      if state.token |> Token.should_refresh?() do
+        Token.get(state.api_key)
+      else
+        {:ok, state.token}
+      end
+
+    with {:ok, token} <- token_res,
+         {:ok, _protocol} <- :gun.await_up(conn_pid),
+         :ok = :gun.flush(conn_pid),
+         ws_ref = :gun.ws_upgrade(conn_pid, state.ws_path, [token |> Token.auth_header()]),
+         :ok <- await_upgrade(conn_pid, ws_ref) do
+      %{state | token: token, ws_ref: ws_ref}
+    else
+      {:error, reason} -> raise "Error while reconnecting: #{inspect(reason)}"
     end
   end
 end
